c1b191dbb0638f9dc64fec1586f79e5d8d11beff
jn-html <marmainjean@gmail.com>
2019-10-03 17:49:33 +0200

    Test GitKraken

--- wilders.js
+++ wilders.js
@@ -4,3 +4,6 @@ console.log(chalk.blue('Bwouno'));
 console.log(chalk.red('Rodolphe'));
 console.log(chalk.yellow('Alex'));
 console.log(chalk.inverse('Eduardo'));
+console.log(chalk.inverse('Eduardo'));
+console.log(chalk.inverse('Eduardo'));
+console.log(chalk.inverse('Eduardo'));
