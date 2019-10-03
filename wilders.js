d14be68b7aec0ed8ef4e318fb94828a179be2415
jn-html <marmainjean@gmail.com>
2019-10-03 17:51:38 +0200

    *belec

--- wilders.js
+++ wilders.js
@@ -4,6 +4,3 @@ console.log(chalk.blue('Bwouno'));
 console.log(chalk.red('Rodolphe'));
 console.log(chalk.yellow('Alex'));
 console.log(chalk.inverse('Eduardo'));
-console.log(chalk.inverse('Eduardo'));
-console.log(chalk.inverse('Eduardo'));
-console.log(chalk.inverse('Eduardo'));
