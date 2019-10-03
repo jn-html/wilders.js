045cb7611de659efa295cf708a489c46e45b02c9
jn-html <marmainjean@gmail.com>
2019-10-03 16:01:36 +0200

    Initial commit

---
+++ wilders.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Bwouno'));
+console.log(chalk.red('Rodolphe'));
+console.log(chalk.yellow('Alex'));
+console.log(chalk.inverse('Eduardo'));
